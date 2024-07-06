8ea0c5780375008e2f8b10b672f017073733247f
Rashid Ma <rashid.makhmud@gmail.com>
2024-07-06 20:47:36 +0200

    added openai

---
+++ src/openai.test.ts
@@ -0,0 +1,71 @@
+import main from './openai'
+import 'dotenv/config'
+
+async function execute(inputObj: any) {
+    const inputJson = JSON.stringify(inputObj)
+    console.log('INPUT:', inputJson)
+    return await main(inputJson)
+}
+
+// Sample body input for a POST request
+const sampleInput = {
+    "untrustedData": {
+        "fid": 2,
+        "url": "https://fcpolls.com/polls/1",
+        "messageHash": "0xd2b1ddc6c88e865a33cb1a565e0058d757042974",
+        "timestamp": 1706243218,
+        "network": 1,
+        "buttonIndex": 2,
+        "castId": {
+            "fid": 226,
+            "hash": "0xa48dd46161d8e57725f5e26e34ec19c13ff7f3b9"
+        }
+    },
+    "trustedData": {
+        "messageBytes": "d2b1ddc6c88e865a33cb1a565e0058d757042974..."
+    }
+}
+
+async function test() {
+    const getResult = await execute({
+        method: 'GET',
+        path: '/ipfs/QmVHbLYhhYA5z6yKpQr4JWr3D54EhbSsh7e7BFAAyrkkMf',
+        queries: {
+            chatQuery: ["What is a decentralized web3?"],
+            openAiModel: ["gpt-4o"]
+        },
+        secret: { openaiApiKey: process.env.OPENAI_API_KEY },
+        headers: {},
+    })
+    console.log('GET RESULT:', JSON.parse(getResult))
+
+    const postResult = await execute({
+        method: 'POST',
+        path: '/ipfs/QmVHbLYhhYA5z6yKpQr4JWr3D54EhbSsh7e7BFAAyrkkMf',
+        queries: {
+            chatQuery: ["When did humans land on the moon?"],
+            openAiModel: ["gpt-4o"]
+        },
+        secret: { openaiApiKey: process.env.OPENAI_API_KEY },
+        headers: {},
+        body: {}
+    })
+    console.log('POST RESULT:', JSON.parse(postResult))
+
+    const testArgsString = JSON.stringify({
+        method: 'GET',
+        path: '/ipfs/QmVHbLYhhYA5z6yKpQr4JWr3D54EhbSsh7e7BFAAyrkkMf',
+        queries: {
+            chatQuery: ["Who are you?"],
+            openAiModel: ["gpt-4o"]
+        },
+        secret: { openaiApiKey: "OPENAI_API_KEY" },
+        headers: {},
+    })
+    console.log(`\nTo test in the SideVM playground go to https://phat.phala.network/contracts/view/0xf0a398600f02ea9b47a86c59aed61387e450e2a99cb8b921cd1d46f734e45409\n\nConnect you polkadot.js account and select 'run_js' with the parameters:\n- engine: SidevmQuickJSWithPolyfill\n- js_code: Source code text of dist/index.ts\n- args: ${testArgsString}`);
+    console.log('Watch video here for to see the visual steps of testing in Sidevm playground: https://www.youtube.com/watch?v=fNqNeLfFFME');
+    console.log(`\nMake sure to replace queries and secret with your values compatible with your AI Agent Contract.`);
+
+}
+
+test().then(() => { }).catch(err => console.error(err)).finally(() => process.exit())
---
+++ src/openai.ts
@@ -0,0 +1,39 @@
+import { Request, Response, route } from './httpSupport'
+import { renderHtml } from './uiSupport'
+
+async function GET(req: Request): Promise<Response> {
+    const secret = req.queries?.key ?? '';
+    const openaiApiKey = req.secret?.openaiApiKey as string;
+    const openAiModel = 'gpt-4o';
+    const query = req.queries.chatQuery[0] as string;
+    let result = '';
+
+    try {
+        const response = await fetch('https://api.red-pill.ai/v1/chat/completions', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${openaiApiKey}`,
+            },
+            body: JSON.stringify({
+                messages: [{ role: "user", content: `${query}` }],
+                model: `${openAiModel}`,
+            })
+        });
+        const responseData = await response.json();
+        result = responseData.choices[0].message.content as string;
+    } catch (error) {
+        console.error('Error fetching chat completion:', error);
+        result = error;
+    }
+
+    return new Response(renderHtml(result));
+}
+
+async function POST(req: Request): Promise<Response> {
+    return new Response('Not Implemented')
+}
+
+export default async function main(request: string) {
+    return await route({ GET, POST }, request)
+}
